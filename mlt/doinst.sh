a00646f3f07c1812210d4ca13376cebe320db1a3
Skin Mox <skin-mox@bol.com.br>
2017-01-20 19:04:17 -0200

    Changed in 2017-01-20

--- mlt/doinst.sh
+++
@@ -1,7 +0,0 @@
-config etc/php.d/mlt.ini.new
-config etc/php.d/mlt.ini.new
-config etc/php.d/mlt.ini.new
-config etc/php.d/mlt.ini.new
-config etc/php.d/mlt.ini.new
-config etc/php.d/mlt.ini.new
-config etc/php.d/mlt.ini.new
